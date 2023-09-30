36cb7965d26ac657ba78129850165e9ba08aac5e
Sofia Rojo <sofiarojo27@gmail.com>
2023-09-29 23:17:55 -0300

    Modificaciones

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import express from "express";
-import { engine } from "express-handlebars";
+import handlebars from "express-handlebars";
 import viewsRouter from "./routes/views.router.js";
 import { __dirname } from "./utils.js";
-const app = express();
+import { Server } from "socket.io";
+import { productsManager } from "./products.manager.js";
 
-console.log(__dirname);
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 //handlebars
-app.engine("handlebars", engine());
+app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
 
@@ -19,6 +20,17 @@ app.use("/", viewsRouter);
 
 const PORT = 8080;
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
   console.log(`Escuchando al puerto ${PORT}`);
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log(`Cliente conectado: ${socket.id}`);
+
+  socket.on("createProduct", async (product) => {
+    const newProduct = await productsManager.createProduct(product);
+    socket.emit("productCreated", newProduct);
+  });
 });
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,29 @@
-console.log("Probando cliente");
+const stocketClient = io();
+const form = document.getElementById("form");
+const inputTitle = document.getElementById("title");
+const inputDescription = document.getElementById("description");
+const inputPrice = document.getElementById("price");
+const table = document.getElementById("table");
+const tableBody = document.getElementById("tableBody");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const product = {
+    title: inputTitle.value,
+    description: inputDescription.value,
+    price: inputPrice.value,
+  };
+  stocketClient.emit("createProduct", product);
+};
+
+stocketClient.on("productCreated", (product) => {
+  const { id, title, description, price } = product;
+  const row = `
+    <tr>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>${description}</td>
+        <td>${price}</td>
+    </tr>`;
+  table.innerHTML += row;
+});
